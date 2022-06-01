c5b8dd76c284f5777046d233196f35bbe9647bdc
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-01 14:21:06 -0300

    Add files via upload

---
+++ Projetos/Dev.Watch/assets/php/storage/produtos.php
@@ -0,0 +1,48 @@
+<?php 
+    $produtos = [
+        "Aquat"=>[
+            ["id"=>1, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Aquat"],
+            ["id"=>2, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Aquat"],
+            ["id"=>3, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Aquat"],
+            ["id"=>4, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Aquat"],
+            ["id"=>5, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Aquat"],
+            ["id"=>6, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Aquat"]
+        ],
+
+        "Executivo"=>[
+            ["id"=>1, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Executivo"],
+            ["id"=>2, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Executivo"],
+            ["id"=>3, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Executivo"],
+            ["id"=>4, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Executivo"],
+            ["id"=>5, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Executivo"],
+            ["id"=>6, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Executivo"]
+        ],
+
+        "Esportivo"=>[
+            ["id"=>1, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Esportivo"],
+            ["id"=>2, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Esportivo"],
+            ["id"=>3, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Esportivo"],
+            ["id"=>4, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Esportivo"],
+            ["id"=>5, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Esportivo"],
+            ["id"=>6, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Esportivo"]
+        ],
+
+        "Premium"=>[
+            ["id"=>1, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Premium"],
+            ["id"=>2, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Premium"],
+            ["id"=>3, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Premium"],
+            ["id"=>4, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Premium"],
+            ["id"=>5, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Premium"],
+            ["id"=>6, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Premium"]
+        ],
+
+        "Vintage"=>[
+            ["id"=>1, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Vintage"],
+            ["id"=>2, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Vintage"],
+            ["id"=>3, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Vintage"],
+            ["id"=>4, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Vintage"],
+            ["id"=>5, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Vintage"],
+            ["id"=>6, "nome"=>"test", "preco"=>"teste", "img"=>"pro_", "categoria"=>"Vintage"]
+        ]
+    ];
+?>
---
+++ Projetos/Dev.Watch/produtoPag.php
@@ -0,0 +1,5 @@
+<?php 
+    require("assets/php/contoller.php");    
+    coletaCookie();
+    header("Location: index.php#produtos");
+?>
