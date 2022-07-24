ca1ce3b15ea1c93fd856cc19a1055a95a9c4b7e9
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-07-24 13:19:20 -0300

    Update bd_site.sql

--- ProfessorMAU/bd_site.sql
+++ ProfessorMAU/bd_site.sql
@@ -15,12 +15,7 @@ CREATE TABLE `tbl_usuario` (
 
 INSERT INTO `tbl_usuario` (`id`, `NomeUsuario`, `SobrenomeUsuario`, `DataNascUsuario`, `EmailUsuario`, `SenhaUsuario`, `CelularUsuario`) VALUES
 (11, 'Kaique', 'Souza Santos', '2006-02-12', 'kaique.tudojoia_3@gmail.com', 'kaique.opa', '11-1234-5678'),
-(12, 'teste', '01', '0000-00-00', 'teste_01', 'opa', '1131231231'),
-(13, 'Cicera Raquel de Souza Santos', '01', '0000-00-00', 'kelssantos07@gmail.com', '12164', '1131231231'),
-(19, 'Cicera Raquel de Souza Santos', '01', '0000-00-00', 'kelssantos072@gmail.com', '', '1131231231'),
-(31, 'Cicera Raquel de Souza Santos', '01', '0000-00-00', 'kelssantos072@1gmail.com', '', '1131231231'),
-(33, 'Cicera Raquel de Souza Santos', '01', '0000-00-00', 'kelssantos071323@gmail.com', '21312321', '1131231231'),
-(34, 'Cicera Raquel de Souza Santos', '01', '0000-00-00', 'kelssantos0171323@gmail.com', 'ewq', '1131231231');
+(12, 'teste', '01', '0000-00-00', 'teste_01', 'opa', '1131231231')
 
 ALTER TABLE `tbl_usuario`
   ADD PRIMARY KEY (`id`),
