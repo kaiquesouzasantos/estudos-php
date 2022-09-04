3b7547b3880bb57a3572fb77530db2ecbc83d2f0
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-09-04 10:06:32 -0300

    Add files via upload

--- Projetos/cadastroDeAlunos/cadastra/cadastra-turma.php
+++ Projetos/cadastroDeAlunos/cadastra/cadastra-turma.php
@@ -1,6 +1,10 @@
 <?php 
     require("../dao/TurmaDAO.php");
 
+    if(empty($_POST['txtNome'])){
+        header("Location: ../formularios/formulario-turma.php");
+    }
+
     TurmaDAO::cadastrar($_POST['txtNome']);
 
     header("Location: ../formularios/formulario-turma.php");
